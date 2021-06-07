b959f278f1c56112aca801764e43ea6a8f500bf8
Loris Birkemeyer <loris.birkemeyer@edu.gobelins.fr>
2021-06-07 15:38:22 +0200

    initialize

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const webpack = require('webpack');
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    port: 8090,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: 'file-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: [
+      '.js',
+      '.vue'
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ]
+};
+
+module.exports = config;
